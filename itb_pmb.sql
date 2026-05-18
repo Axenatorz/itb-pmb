ca5e445790bcd21cb1145401bfd144bbf2a08e5c
Axenzator <akunaldi141420@gmail.com>
2026-05-18 14:39:07 +0700

    fourcommit

--- itb_pmb.sql
+++ itb_pmb.sql
@@ -1,31 +1,8 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Waktu pembuatan: 26 Apr 2026 pada 10.07
--- Versi server: 10.4.32-MariaDB
--- Versi PHP: 8.2.12
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `itb_pmb`
---
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `applicants`
---
 
 CREATE TABLE `applicants` (
   `id` int(11) NOT NULL,
